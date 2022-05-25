bfcfe7ad706a0dbf9c0024f12b35f2c2eb0310ac
Muhmad K. Fauzi <muhmad@live.no>
2022-05-25 23:07:39 +0200

    Added fixed all , delete, put, post and get - fully working api

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+
 from db.moduls import Magasin, session
 
 apiroutes = Blueprint('apiroutes', __name__)
@@ -23,19 +24,46 @@ def get_magasin(id):
                     "fylling_TWh": magasin.fylling_twh, "fyllingsgrad_forrige_uke": magasin.fyllingsgrad_forrige_uke, "endring_fyllingsgrad": magasin.endring_fyllingsgrad, "latitude": magasin.latitude, "longitude": magasin.longitude, "name": magasin.name})
 
 
-# @apiroutes.route('/magasin', methods=['POST'])
-# def add_magasin():
-#     magasin = Magasin(dato_Id=request.json["dato_Id"], iso_uke=request.json["iso_uke"], fyllingsgrad=request.json["fyllingsgrad"], kapasitet_TWh=request.json["kapasitet_TWh"], fylling_TWh=request.json["fylling_TWh"],
-#                       fyllingsgrad_forrige_uke=request.json["fyllingsgrad_forrige_uke"], endring_fyllingsgrad=request.json["endring_fyllingsgrad"], latitude=request.json["latitude"], longitude=request.json["longitude"], name=request.json["name"])
-#     session.add(magasin)
-#     session.commit()
-#     session.close()
-#     return {"Name": magasin.name}
+@apiroutes.route('/magasin', methods=['POST'])
+def add_magasin():
+    magasin = Magasin(iso_uke=request.json["iso_uke"], fyllingsgrad=request.json["fyllingsgrad"], kapasitet_twh=request.json["kapasitet_twh"], fylling_twh=request.json["fylling_twh"],
+                      fyllingsgrad_forrige_uke=request.json["fyllingsgrad_forrige_uke"], endring_fyllingsgrad=request.json["endring_fyllingsgrad"], latitude=request.json["latitude"], longitude=request.json["longitude"], name=request.json["name"])
+    session.add(magasin)
+    session.commit()
+    return {"Name": magasin.name, "massage": "Er lagt til"}
 
 
-# @apiroutes.route("/magasin/<id>", methods="DELETE")
-# def delete_magasin(id):
-#     magasin = session.query(Magasin).filter(Magasin.id == id).first()
-#     session.delete(magasin)
-#     session.commit()
-#     session.close()
+@apiroutes.route('/magasin/<id>', methods=['PUT'])
+def update_magasin(id):
+    magasin_id = session.query(Magasin).filter(Magasin.id == id).first()
+
+    iso_uke = request.json["iso_uke"]
+    fyllingsgrad = request.json["fyllingsgrad"]
+    kapasitet_twh = request.json["kapasitet_twh"]
+    fylling_twh = request.json["fylling_twh"]
+    fyllingsgrad_forrige_uke = request.json["fyllingsgrad_forrige_uke"]
+    endring_fyllingsgrad = request.json["endring_fyllingsgrad"]
+    latitude = request.json["latitude"]
+    longitude = request.json["longitude"]
+    name = request.json["name"]
+    magasin_id.iso_uke = iso_uke
+    magasin_id.fyllingsgrad = fyllingsgrad
+    magasin_id.kapasitet_twh = kapasitet_twh
+    magasin_id.fylling_twh = fylling_twh
+    magasin_id.fyllingsgrad_forrige_uke = fyllingsgrad_forrige_uke
+    magasin_id.endring_fyllingsgrad = endring_fyllingsgrad
+    magasin_id.latitude = latitude
+    magasin_id.longitude = longitude
+    magasin_id.name = name
+    session.commit()
+    return {"Name": magasin_id.name, "massage": "Er oppdatert"}
+
+
+@apiroutes.route("/magasin/<id>", methods=["DELETE"])
+def delete_magasin(id):
+    magasin = session.query(Magasin).filter(Magasin.id == id).first()
+    if magasin is None:
+        return {"erro": "Magasin Not Found"}
+    session.delete(magasin)
+    session.commit()
+    return {"Name": magasin.name, "massage": "Er slettet"}
--- test.py
+++
@@ -1,6 +0,0 @@
-from db.moduls import Magsin, session
-
-test = session.query(Magsin).all()
-
-for tests in test:
-    print(tests)
